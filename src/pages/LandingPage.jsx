e6f64063de455c0b5a474006919f64f4dd0cd802
MD Shami Ahmad <mdshamiahmad005@gmail.com>
2026-08-17 16:45:43 +0530

    Fix SeoHead import ReferenceError causing blank screen on homepage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import SeoHead from '../components/SeoHead';
 import Navbar from '../components/Navbar';
 import {
   Printer,
